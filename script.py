6aa69cb3153bb2f6967f859aad44d501dc201826
hindawi <ahmedhindawi2001@gmail.com>
2023-01-31 20:45:08 +0200

    created a seed4me account successfully

--- script.py
+++ script.py
@@ -4,6 +4,14 @@ from selenium.webdriver.common.by import By
 def wait(driver):
     driver.implicitly_wait(30)
 
+def generate_password(n):
+    import random
+    import string
+    password = ''
+    for i in range(n):
+        password += random.choice(string.ascii_letters + string.digits + string.punctuation + string.digits)
+    return password
+
 mohml = webdriver.Chrome()
 mohml.get("https://www.mohmal.com/")
 wait(mohml)
@@ -12,4 +20,32 @@ create_account.click()
 wait(mohml)
 email_element = mohml.find_element(By.XPATH, "//*[@id=\"email\"]/div[1]")
 email = email_element.text
-print(email)
+
+
+driver = webdriver.Chrome()
+driver.get("https://seed4.me/")
+wait(driver)
+
+my_account = driver.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div/div/div/ul/li[8]/a")
+my_account.click()
+wait(driver)
+
+create_seed4me_account = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/a[2]")
+create_seed4me_account.click()
+wait(driver)
+
+email_textbox = driver.find_element(By.XPATH, "//*[@id=\"UserUsername\"]")
+email_textbox.send_keys(email)
+password_textbox = driver.find_element(By.XPATH, "//*[@id=\"UserPassword\"]")
+password = generate_password(10)
+password_textbox.send_keys(password)
+confirm_password_textbox = driver.find_element(By.XPATH, "//*[@id=\"UserConfirmPassword\"]")
+confirm_password_textbox.send_keys(password)
+confirm = driver.find_element(By.XPATH, "//*[@id=\"UserAccept\"]")
+confirm.click()
+
+register = driver.find_element(By.XPATH, "//*[@id=\"UserRegisterForm\"]/div[8]/div/input")
+register.click()
+
+while True:
+    pass
