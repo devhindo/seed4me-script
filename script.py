e7dbbefe0b28fb551f037ce131071f298c653a0d
hindawi <ahmedhindawi2001@gmail.com>
2023-01-31 22:07:56 +0200

    confirmation mail sent successfully

--- script.py
+++ script.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
-def wait(driver):
-    driver.implicitly_wait(30)
+
+def wait(seed):
+    seed.implicitly_wait(30)
 
 def generate_password(n):
     import random
@@ -12,39 +14,42 @@ def generate_password(n):
         password += random.choice(string.ascii_letters + string.digits + string.punctuation + string.digits)
     return password
 
-mohml = webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument("--incognito")
+
+mohml = webdriver.Chrome(chrome_options=chrome_options)
 mohml.get("https://www.mohmal.com/")
 wait(mohml)
 create_account = mohml.find_element(By.XPATH, "//*[@id=\"rand\"]")
 create_account.click()
 wait(mohml)
+
 email_element = mohml.find_element(By.XPATH, "//*[@id=\"email\"]/div[1]")
 email = email_element.text
 
+seed = webdriver.Chrome(chrome_options=chrome_options)
+seed.get("https://seed4.me/")
+wait(seed)
 
-driver = webdriver.Chrome()
-driver.get("https://seed4.me/")
-wait(driver)
-
-my_account = driver.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div/div/div/ul/li[8]/a")
+my_account = seed.find_element(By.XPATH, "/html/body/div[2]/div/div[1]/div/div/div/ul/li[8]/a")
 my_account.click()
-wait(driver)
+wait(seed)
 
-create_seed4me_account = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/a[2]")
+create_seed4me_account = seed.find_element(By.XPATH, "/html/body/div[3]/div/div/a[2]")
 create_seed4me_account.click()
-wait(driver)
+wait(seed)
 
-email_textbox = driver.find_element(By.XPATH, "//*[@id=\"UserUsername\"]")
+email_textbox = seed.find_element(By.XPATH, "//*[@id=\"UserUsername\"]")
 email_textbox.send_keys(email)
-password_textbox = driver.find_element(By.XPATH, "//*[@id=\"UserPassword\"]")
+password_textbox = seed.find_element(By.XPATH, "//*[@id=\"UserPassword\"]")
 password = generate_password(10)
 password_textbox.send_keys(password)
-confirm_password_textbox = driver.find_element(By.XPATH, "//*[@id=\"UserConfirmPassword\"]")
+confirm_password_textbox = seed.find_element(By.XPATH, "//*[@id=\"UserConfirmPassword\"]")
 confirm_password_textbox.send_keys(password)
-confirm = driver.find_element(By.XPATH, "//*[@id=\"UserAccept\"]")
+confirm = seed.find_element(By.XPATH, "//*[@id=\"UserAccept\"]")
 confirm.click()
 
-register = driver.find_element(By.XPATH, "//*[@id=\"UserRegisterForm\"]/div[8]/div/input")
+register = seed.find_element(By.XPATH, "//*[@id=\"UserRegisterForm\"]/div[8]/div/input")
 register.click()
 
 while True:
